4862d5219bc3609e51c28ea77f3c9275ea886b2d
JayShang <yijieshang@gmail.com>
2018-06-08 14:31:06 +0800

    审核被拒绝原因记录

---
+++ IOS项目审核反馈总结/IOS项目审核反馈总结/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IOS项目审核反馈总结
+//
+//  Created by caihongguang on 2018/6/8.
+//  Copyright © 2018年 SYJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        
+        
+    }
+
+  
+
+}
+
